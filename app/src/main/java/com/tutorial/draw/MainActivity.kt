9e0ebea962f4b00e292b82b06fa6ae6590ab80ae
giangthao <thaogiang.90@gmail.com>
2022-07-13 20:14:03 +0700

    121. Selecting the Brush Size From A Custom Dialog That we Created

--- app/src/main/java/com/tutorial/draw/MainActivity.kt
+++ app/src/main/java/com/tutorial/draw/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.tutorial.draw
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     private  var drawingView:DrawingView? = null
@@ -10,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+    }
+    private  fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush Size")
+        val smallBtn : ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        smallBtn.setOnClickListener(View.OnClickListener {
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        )
+
     }
 }
