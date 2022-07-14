aa67a773ebead6d3daf7d75a398e6baaf30ecacf
giangthao <thaogiang.90@gmail.com>
2022-07-14 15:04:01 +0700

    123. Adding the color pallet using custom drawables that we created

--- app/src/main/java/com/tutorial/draw/MainActivity.kt
+++ app/src/main/java/com/tutorial/draw/MainActivity.kt
@@ -6,14 +6,24 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private  var drawingView:DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColor = findViewById<LinearLayout>(R.id.ll_paint_collors)
+        mImageButtonCurrentPaint = linearLayoutPaintColor[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this,R.drawable.pallet_preset)
+        )
         val ib_brush :ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener{
             showBrushSizeChooserDialog()
